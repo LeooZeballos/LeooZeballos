270a573cf146f6eacfa4b16a2d762d9140067df8
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-17 15:14:59 -0300

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
-import './about.css'
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { BsFolderFill } from 'react-icons/bs';
+import './about.css';
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>A year working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>Lots of 'em!</small>
+            </article>
+            <article className="about__card">
+              <BsFolderFill className="about__icon" />
+              <h5>Projects</h5>
+              <small>At least 2</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, expedita. At consequatur, iure ex dolore suscipit tempore id voluptate. Perspiciatis maiores ullam nostrum quia molestias omnis qui sequi rerum eligendi.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
