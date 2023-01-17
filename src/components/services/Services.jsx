d5d7c1fdbe211a81b99296c15371d7c391a0cc76
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-17 16:13:36 -0300

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import Service from "./Service";
+import "./services.css";
 
 const Services = () => {
-  return (
-    <section>Services</section>
-  )
-}
+  const services = [
+    {
+      title: "Web Development",
+      items: ["React", "JavaScript", "HTML5", "CSS3"],
+    },
+    {
+      title: "API Development",
+      items: ["REST", "Spring Boot"],
+    },
+    {
+      title: "Database",
+      items: ["MySQL", "PostgreSQL", "SQLite"],
+    },
+    {
+      title: "Server",
+      items: ["Linux", "Apache", "Node.js"],
+    },
+  ];
 
-export default Services
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        {services.map((service, index) => (
+          <Service key={index} service={service} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
