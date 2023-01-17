24aa6d310a36f4fa3a91c2ac6d3974090d4d5a93
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-17 14:09:57 -0300

    header component + react icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 import './header.css'
+import Socials from './Socials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Leonel Zeballos</h1>
+        <h5 className="text-light">Software Developer</h5>
+        <CTA />
+        <Socials />
+
+        <div className="me">
+          <img src={ME} alt="Leonel Zeballos" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
