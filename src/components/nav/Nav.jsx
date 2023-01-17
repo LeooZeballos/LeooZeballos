c31f220f5f09bb83cca2b27322627ce47ad1db8b
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-17 14:30:22 -0300

    nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './nav.css'
+import React, { useState } from "react";
+import { ImHome } from "react-icons/im";
+import { FaUserAlt } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
+import { AiFillProject } from "react-icons/ai";
+import { IoMdContact } from "react-icons/io";
+import "./nav.css";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const [active, setActive] = useState("#header");
 
-export default Nav
+  return (
+    <nav>
+      <a
+        href="#header"
+        className={active === "#header" ? "active" : ""}
+        onClick={() => setActive("#header")}
+      >
+        <ImHome />
+      </a>
+      <a
+        href="#about"
+        className={active === "#about" ? "active" : ""}
+        onClick={() => setActive("#about")}
+      >
+        <FaUserAlt />
+      </a>
+      <a
+        href="#experience"
+        className={active === "#experience" ? "active" : ""}
+        onClick={() => setActive("#experience")}
+      >
+        <MdOutlineWork />
+      </a>
+      <a
+        href="#portfolio"
+        className={active === "#portfolio" ? "active" : ""}
+        onClick={() => setActive("#portfolio")}
+      >
+        <AiFillProject />
+      </a>
+      <a
+        href="#contact"
+        className={active === "#contact" ? "active" : ""}
+        onClick={() => setActive("#contact")}
+      >
+        <IoMdContact />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
