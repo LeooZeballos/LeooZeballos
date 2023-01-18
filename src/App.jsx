732c0f64fa95d9d6515e00765d0914e79e19c6e1
LeooZeballos <zeballosleonel3@gmail.com>
2023-01-18 12:52:58 -0300

    commented out testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
-import Testimonials from "./components/testimonials/Testimonials";
+// import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -19,7 +19,7 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
